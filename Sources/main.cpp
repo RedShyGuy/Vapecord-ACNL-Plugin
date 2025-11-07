3cb88b6999cb465ed9980b275ad208be5e4d8152
Spencer Everly <219582684+DawnRiveraTheQueen@users.noreply.github.com>
2025-11-06 20:44:46 -0600

    Remove unnecessary new line on credits

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -16,7 +16,7 @@
 
 namespace CTRPluginFramework {
 	static const std::string Note = "Creator: Lukas#4444 (RedShyGuy) \n\n"
-									"Code Credits: Nico, Jay, Levi, Slattz, Kominost, Elominator, Jon &\nDaniElectra (Pretendo), and more \n\n"
+									"Code Credits: Nico, Jay, Levi, Slattz, Kominost, Elominator, Jon & DaniElectra (Pretendo), and more\n\n"
 									"Translators: みるえもん & みなと(Japanese), im a book(spanish), Fedecrash02(italian), Youssef, Arisa, & Lenoch(french), bkfirmen & Toby(german), Soopoolleaf(korean) \n\n"
 									"" << Utils::Format("Discord: %s", DISCORDINV);
 
